12435fc27c47a425f857e5ff79f49d27b3ee1e28
Antonio-a1324 <anthony.v.rodrigues@aluno.senai.br>
2026-09-24 10:03:33 -0300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,7 @@
 
 constexpr float TEMPERATURA_LIGA = 30.0;
 constexpr float TEMPERATURA_DESLIGA = 31.0;
-constexpr unsigned long INTERVALO_LEITURA_MS = 60000;
+constexpr unsigned long INTERVALO_LEITURA_MS = 10000;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
